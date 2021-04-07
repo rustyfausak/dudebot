f7d2add80c13735778b2b951b47f21f0d31dfa68
clockberg <rustyfausak@gmail.com>
2021-04-07 13:29:59 -0500

    cache markov model

--- markov.py
+++ markov.py
@@ -1,5 +1,9 @@
 import markovify
 import sys
+import os
+import time
+
+cache_lifetime_minutes = 60
 
 if len(sys.argv) != 2:
 	print("Usage: markov.py <channel_id>");
@@ -7,17 +11,38 @@ if len(sys.argv) != 2:
 
 channel_id = sys.argv[1]
 
-# Get raw text as string.
-with open("corpora/" + channel_id + ".txt") as f:
-	text = f.read()
+# Check the cache file
+cachefile = "corpora/" + channel_id + "-cache.json"
+if os.path.exists(cachefile) and os.path.getmtime(cachefile) > time.time() - cache_lifetime_minutes * 60:
+	# Load cache file
+	with open(cachefile) as f:
+		cache = f.read()
 
-# Build the model.
-text_model = markovify.NewlineText(text)
+	# Build the model
+	text_model = markovify.Text.from_json(cache)
+else:
+	# Check that the corpus exists
+	corpusfile = "corpora/" + channel_id + ".txt"
+	if not os.path.exists(corpusfile):
+		exit(0)
 
-# Try five times for a randomly-generated sentence
-for i in range(5):
-	s = text_model.make_sentence()
-	if (not s):
-		continue
-	print(s)
-	exit(0)
+	# Get raw text as string
+	with open(corpusfile) as f:
+		text = f.read()
+
+	# Build the model
+	text_model = markovify.NewlineText(text)
+
+	# Save model cache
+	model_json = text_model.to_json()
+	f = open(cachefile, "w")
+	f.write(model_json)
+	f.close()
+
+	# Try five times for a randomly-generated sentence
+	for i in range(5):
+		s = text_model.make_sentence()
+		if (not s):
+			continue
+		print(s)
+		exit(0)
